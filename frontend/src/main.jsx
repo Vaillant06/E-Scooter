3623012f67f6ecf0416359f235a2ebad246a7247
SREENATH G <sreenath2410530@ssn.edu.in>
2026-01-03 11:40:47 +0530

    Removed snowfall

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
-import SnowFall from 'react-snowfall';
 
 import App from './App.jsx';
 
@@ -10,7 +9,6 @@ import "./styles/index.css";
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <SnowFall />
         <App />
     </BrowserRouter>
   </StrictMode>,
